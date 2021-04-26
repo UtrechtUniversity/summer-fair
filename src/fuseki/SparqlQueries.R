f59d56f08e39eb83a6149fb518512f5f555373cc
Slavc001 <Nasho012021>
2021-04-26 15:56:17 +0200

    Fuseki triplestore runs from docker

---
+++ src/fuseki/SparqlQueries.R
@@ -0,0 +1,28 @@
+library(SPARQL)
+
+endpoint <- "http://localhost:3030/mydataset/query"
+
+get.dates.swab <- function(){
+  #SPARQL query to get the experiment dates of SWAB sample
+  sparql <- " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX tr: <http://www.semanticweb.org/trans_experiment#>
+SELECT distinct ?date  
+	WHERE { ?subject tr:experimentDay ?date;
+ 		          tr:hasSample ?sample.
+?sample rdf:type tr:SwabSample;
+tr:hasResult 'positive';
+tr:involves ?chicken.
+
+?chicken  rdf:type tr:Individual;
+tr:type ?type.
+ }
+order by ?date"
+  
+  return( sparql)
+}
+
+swab.dates <- SPARQL(url = endpoint,query=get.dates.swab())$results
+write.csv(x=swab.dates,file='dates.csv')
