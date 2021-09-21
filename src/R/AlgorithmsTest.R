8326178dec69fd16a2fb9990f0b04baf857c6536
Fischer <E.A.J.Fischer@uu.nl>
2021-09-21 17:08:48 +0200

    Started working on local algorithm based on output as obtained from query

--- src/R/AlgorithmsTest.R
+++ src/R/AlgorithmsTest.R
@@ -14,7 +14,7 @@
 ## from CRAN and then loaded.
 
 ## First specify the packages of interest
-packages = c("gridExtra","rstan","shinystan","rstanarm")
+packages = c()
 
 ## Now load or install&load all
 package.check <- lapply(
@@ -27,4 +27,23 @@ package.check <- lapply(
   }
 )
 
-####
+source("LocalAlgorithm.R")
+
+
+##create some mock data####
+mockdata <- data.frame(
+  chickid = rep(c(1:20),each = 5),         # 20 animals
+  time = rep(c(1:5), 20),                  # 5 times
+  location = rep(c("A","B"),each = 50),     # 2 locations
+  type = rep(c(rep("I",25),rep("S",25)),2),# first 5 animals per group I  
+  treatment = "none",                      # no treatment  
+  sample = c(rbinom(25,1,.7),rbinom(25,1,.3),
+               rbinom(25,1,.7),rbinom(25,1,0.3))               # random positive and negative samples
+
+)
+
+boxplot(mockdata$sample~ mockdata$type+mockdata$location, type = "l" )
+
+
+cor(x =mockdata$sample[1:50], y = mockdata$sample[51:100])
+
---
+++ src/R/LocalAlgorithm.R
@@ -0,0 +1,30 @@
+##########################################################
+#                                                        #
+#  Local algorithm estimation of transmission paramaters #
+#                                                        #
+#                                                        #
+#                  Author:Egil A.J. Fischer              #
+#                  Contact: e.a.j.fischer@uu.nl          #
+#                  Creation date: 21-9-2021              #
+##########################################################
+
+## install and load packages ####
+## First specify the packages of interest
+packages = c("SPARQL",
+             "rstan",
+             "shinystan",
+             "rstanarm")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+###
+
