4b6776ac07e3da228676be6e180980e0deb508a5
eslavco <e.slavco@gmail.com>
2021-10-15 10:57:42 +0200

    WIP: restrucuring the code to be able to work with tidy data

--- src/create_ontology/map_ontology/main.py
+++ src/create_ontology/map_ontology/main.py
@@ -1,70 +1,42 @@
 """Create linked data set
-
 Create instances and populate ontology schema.
 The instances are represented as RDF triples (linked data).
 Required arguments:
 Data in tabular format
 Ontology schema
 Mapping file
-
 """
 
-import pandas as pd
 import click
+
+from dataset import Dataset
 from mappings import Mappings
 from ontology import Ontology
-from pathlib import Path
+
 
 @click.command()
-@click.option('--config', required=True,type=Path,
-			  help='path to mapping file')
-@click.option('--filename', required=True,type=Path,
-			  help='path to dataset')
-@click.option('--outdir',type=Path, default='linked_data',
-			  help='path to output folder')
-
-def read_tabular(filename:Path) -> pd.DataFrame:
-    """Read tabular data in Dataframe"""
-    try:
-        if filename.suffix in ('.xlsx','.xls'):
-            dataset = pd.read_excel(filename).fillna(method='ffill',axis=0).fillna('')
-        elif filename.suffix == '.csv':
-            dataset = pd.read_csv(filename,delimiter=':').fillna(method='ffill',axis=0).fillna('')
-    except (UnicodeDecodeError,IndexError,ValueError) as e:
-        print(f'Failed reading {filename} with error msg {e} ')
-    except FileNotFoundError:    
-        print(f'File {filename} does not exist')
-
-    return dataset
-
-def check_outdir(outdir:Path):
-    """Check and/or create outdir"""
-    if outdir.exists():
-        if outdir.is_file():
-            raise RuntimeError('Output path must be a folder')
-    else:
-        outdir.mkdir(parents=True, exist_ok=True)
-
-
-def main(config: Path,filename: Path,outdir: Path):
+@click.option('--config', required=True,
+              help='path to mapping file')
+@click.option('--filename', required=True,
+              help='path of the dataset')
+@click.option('--worksheet', required=False,
+              help='name of the worksheet to ')
+def main(config, filename, worksheet):
+    # File with ontology
     ont_file = 'trans_ont.owl'
 
-    dataset = read_tabular(filename)
-    columns = dataset.columns.values.tolist()
-
-    # create mapping and ontology class
-    mappings = Mappings(config, columns)
+    mappings = Mappings(config)
     ontology = Ontology(ont_file)
+    dataset = Dataset(filename, mappings)
 
-    for _, row in dataset.iterrows():
+    for _, row in dataset.tidy_dataset.iterrows():
         # check for the required field
         # if it doesn't exit then run it for each row
         if mappings.required_field is None or row[mappings.required_field]:
-            ontology.populate_ontology(mappings, row,columns)
+            ontology.populate_ontology(mappings, row)
 
-    # save populated ontology
-    outdir = check_outdir()
-    ontology.save_ontology(f'{outdir/filename.stem}.ttl')
+    ontology.save_ontology('data/populated_ont.ttl')
+
 
 if __name__ == '__main__':
     main()
