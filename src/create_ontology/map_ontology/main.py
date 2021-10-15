af18d43f7273a0d63d512ceb8588e42cbe9f4de6
eslavco <e.slavco@gmail.com>
2021-10-15 10:57:42 +0200

    WIP: restrucuring the code to be able to work with tidy data

--- src/create_ontology/map_ontology/main.py
+++ src/create_ontology/map_ontology/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from mappings import Mappings
 from ontology import Ontology
+from collections import namedtuple
+import utils
 
 
 def main():
@@ -9,21 +11,49 @@ def main():
     filename = 'data_maldi_UU_2_updated.xlsx'
     ont_file = 'trans_ont.owl'
 
-    # parse dataset
-    dataset = pd.read_excel(filename).fillna(method='ffill', axis=0).fillna('')
-    columns = dataset.columns.values.tolist()
-
-    # create mapping and ontology class
-    mappings = Mappings(config, columns)
+    mappings = Mappings(config)
     ontology = Ontology(ont_file)
 
-    for _, row in dataset.iterrows():
-        # check for the required field
-        # if it doesn't exit then run it for each row
-        if mappings.required_field is None or row[mappings.required_field]:
-            ontology.populate_ontology(mappings, row)
+    # Check if the merged field is specified in config
+    # If yes, merge the spreadsheet based on merged field
+    merge_field = mappings.mappings.get('merge_spreadsheets_on', '')
+    if merge_field:
+        dataset = utils.merge_spreadsheets(filename, merge_field)
+    else:
+        dataset = pd.read_excel(filename)
 
-    ontology.save_ontology('test.ttl')
+    dataset = dataset.fillna('')
+
+    columns = dataset.columns.values.tolist()
+    reocur_columns_dict = utils.get_dataset_reocur(mappings.reocur_mappings, columns)
+    # check if in the reocur_columns there are columns with meaningful data in column name
+    # for example if (.*) is in column name. If yes, create new columns with meaningful data in df
+
+    if reocur_columns_dict and utils.values_in_columnames(reocur_columns_dict):
+        utils.create_new_columns_in_df(dataset, reocur_columns_dict)
+
+    reocur_columns = [v for value in reocur_columns_dict.values() for v in value]
+    unique_columns = list(set(columns).difference(reocur_columns))
+
+    stacked_df = utils.transform_dataset(dataset, mappings, unique_columns, reocur_columns_dict).fillna('')
+
+    # update values
+    update_columns = mappings.update_values
+    if update_columns:
+        for column in update_columns:
+            values = column['values']
+            column_name = column['column_name']
+            for new_value, old_values in values.items():
+                stacked_df.loc[stacked_df[column_name].isin(old_values), [column_name]] = str(new_value)
+
+        for _, row in dataset.iterrows():
+            # check for the required field
+            # if it doesn't exit then run it for each row
+            if mappings.required_field is None or row[mappings.required_field]:
+                ontology.populate_ontology(mappings, row, columns)
+
+        ontology.save_ontology('populated_ont.owl')
+
 
 if __name__ == '__main__':
     main()
