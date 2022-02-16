cc18a9df503519a05f34e3b9fa715505ec20bfce
eslavco <e.slavco@gmail.com>
2022-02-16 17:23:19 +0100

    Updated docker for triplestore

--- src/fuseki/docker.sh
+++ src/fuseki/docker.sh
@@ -1,27 +1,23 @@
 docker run --detach --name fuseki-data --volume /fuseki busybox
 docker run --detach --name fuseki-app -p 3030:3030 \
   -e ADMIN_PASSWORD=admin \
-  -e FUSEKI_DATASET_1=dataset_a\
-  -e FUSEKI_DATASET_2=dataset_b\
-  -e FUSEKI_DATASET_3=dataset_c\
+  -e FUSEKI_DATASET_1=mydataset\
   --volumes-from fuseki-data stain/jena-fuseki:3.14.0
 
 docker stop fuseki-app
 
 docker run -d --name fuseki-loadsh-use-fuseki-data \
   --volumes-from fuseki-data \
-  --volume "$(pwd)/ontology:/staging" \
+  --volume "$(pwd)/src/create_ontology/map_ontology/data:/staging" \
   stain/jena-fuseki:3.14.0 \
-  ./load.sh dataset_a  trans_experiment_A.ttl
+  ./load.sh mydataset  populated_ont.ttl
 
 docker stop fuseki-loadsh-use-fuseki-data
 
-
 docker start fuseki-app
-
 
 # Run R script within r-studio container
 
-docker build -t rproject .
+#docker build -t rproject .
 
-docker run --name sparql rproject
+#docker run --name sparql rproject
