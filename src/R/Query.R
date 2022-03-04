973f15e8d84d24c6fce554eac7738bfe0bcd78a6
eslavco <e.slavco@gmail.com>
2022-03-04 14:36:55 +0100

    Updated query

--- src/R/Query.R
+++ src/R/Query.R
@@ -16,20 +16,25 @@ get.data <- function(endpoint){
             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
             PREFIX tr: <http://www.thomsonreuters.com/>
             PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-            SELECT ?round ?ex_day ?group ?level1 ?level2 ?level3 ?host_id ?treatment ?innoculationStatus ?sample_measure ?sample_result ?pathogen_name WHERE {
+            SELECT ?round ?ex_day ?inoculationHour ?group ?level1 ?level2 ?level3 ?host_id ?treatment ?inoculationStatus ?sample_measure ?sample_result ?sample_type ?pathogen_name WHERE {
               ?experiment a :Experiment;
                           :experimentDay ?ex_day;
                           :hasMeasurement ?measurement.
               optional {?experiment :repetition ?round;}
+
+
               ?measurement a :Measurement;
                            :hasHost ?host;
                            :hasSample ?sample.
               ?host :id ?host_id;
                     :treatment ?treatment;
-                    :innoculationStatus ?innoculationStatus;
+                    :inoculationStatus ?inoculationStatus;
                     :locatedIn ?env.
               ?env  :groupNumber ?group. 
-              optional{?env :level1 ?level1;}
+ optional{?experiment :hasInoculation ?inoculation.
+    ?inoculation :involves ?host.
+ ?inoculation	 :experimentHour ?inoculationHour.}
+              optional{?env :level1 ?level1.}
               optional{?env :level2 ?level2;}
               optional{?env :level3 ?level3;}
               optional{ ?measurement :experimentHour ?ex_hour. }
@@ -37,10 +42,11 @@ get.data <- function(endpoint){
                            :hasQuantity ?quantity. 
                 ?quantity om:hasValue ?measure.
                 ?measure om:hasNumericalValue ?sample_measure.
-              }
-               optional{ ?sample  :hasPathogen ?pathogen. 
+              } 
+			   ?sample :hasType ?sample_type.
+               optional{?sample  :hasPathogen ?pathogen. 
                 ?pathogen :name ?pathogen_name }
               optional {?sample :hasResult ?sample_result.}
-            } "
+            }"
   return(SPARQL(url = endpoint,query=sparql)$results)
 }
