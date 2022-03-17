859773ee6a766adb55a256a65c09affd75e544d7
eslavco <e.slavco@gmail.com>
2022-03-17 10:54:25 +0100

    updated script with new dataset names and function from LocalAlgorithm

--- src/R/TutorialAlgorithms.R
+++ src/R/TutorialAlgorithms.R
@@ -28,33 +28,18 @@ lapply(
 # scripts should be in subfolder "src/R" 
 source("src/R/DataManipulationRules.R")    #Data manipulation rules are pre- or user defined
 source("src/R/LocalAlgorithm.R")           #Estimation methods
-source("src/R/Query.r")                    #Query function
+source("src/R/Query.r") #Query function
+source("src/R/GlobalAlgorithm.R")
 
 # get data ####
-dataA<- get.data("http://localhost:3030/datasetA")
-dataB<- get.data("http://localhost:3030/datasetB")
+dataA<- get.data("http://localhost:3030/mydataset")
+dataB<- get.data("http://localhost:3031/mydataset")
 
 #run local algorithm for each data set ####
 #run analysis over data set A
-localA <- analyseTransmission(inputdata = dataA,           #data set
-                              rule = rule.sinceany.cutoff, #rule to determine infection status
-                              var.id = c("sample_measure"),#variable defining infection status
-                              method = "glm",              #estimation method
-                              cutoff = 0,                  #cutoff value for infection status
-                              preventError = TRUE,         #TRUE = remove entries with > 1 case but FOI = 0
-                              covars = "treatment",        #co variants 
-                              reference = "control",       #reference category for multivariable estimation
-                              control = "0")               #value of control treatment
+localA <- get.local.trasmission(dataA)
 #run analysis over data set B
-localB<- analyseTransmission(inputdata = dataB,            #data set
-                             rule = rule.sinceany.recode,  #rule to determine infection status
-                             var.id = c("sample_result"),  #variable defining infection status
-                             method = "glm",               #estimation method
-                             codesposnegmiss = c("+","-","NA"), #values determining infection status pos, neg of missing
-                             preventError = TRUE,          #TRUE = remove entries with > 1 case but FOI = 0
-                             covars = "treatment",         #co variants 
-                             reference = "control",        #reference category for multivariable estimation
-                             control = "")                 #value of control treatment
+localB<- get.local.trasmission(dataB)
 
 #combine estimates of control group with standard meta-analysis techniques ####
 metaana <- combine.estimates.glm(list(localA,localB),
