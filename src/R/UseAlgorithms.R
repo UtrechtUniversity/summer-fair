15e302c19641260cdbf9cf55ebf44396c58267f0
Fischer <egil@egilfischer.nl>
2022-08-29 08:58:32 +0200

    Added two data sets

--- src/R/UseAlgorithms.R
+++ src/R/UseAlgorithms.R
@@ -37,12 +37,20 @@ dataB<- get.data("http://localhost:3030/datasetB")
 dataC<- get.data("http://localhost:3030/datasetC")
 dataD<- get.data("http://localhost:3030/datasetD")
 
+#data mapped at WBVR
+dataE<- get.data("http://localhost:3030/datasetE")
+dataF<- get.data("http://localhost:3030/datasetF")
+
 #run local algorithm for each data set ####
 localA <- get.local.transmission(dataA)
 localB <- get.local.transmission(dataB)
 
 localC <- get.local.transmission(dataC)
 localD <- get.local.transmission(dataD)
+
+#data mapped at WBVR
+localE <- get.local.transmission(dataE) #Cecarelli
+localF <- get.local.transmission(dataF) #Van Bunnik
 
 
 #combine estimates of control group with standard meta-analysis techniques ####
