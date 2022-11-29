dcab58fcc680b429e5bcd3bf9ddc4d4547a7aff3
eslavco <e.slavco@gmail.com>
2022-11-29 09:47:57 +0100

    Updated the algorithm to run from combined dockerfile

--- src/R/LocalAlgorithmDocker.R
+++ src/R/LocalAlgorithmDocker.R
@@ -4,16 +4,20 @@
 # Created on: 08/07/2022
 
 
-source("DataManipulationRules.R")    #Data manipulation rules are pre- or user defined
-source("LocalAlgorithm.R") #Estimation methods
-source("GlobalAlgorithm.R")
-source("Query.R")  #Query function
+source("src/R/DataInterpretationRules.R")    #Data manipulation rules are pre- or user defined
+source("src/R/LocalAlgorithm.R") #Estimation methods
+source("src/R/GlobalAlgorithm.R")
+source("src/R/Query.R")  #Query function
 packageVersion('meta')
 # get data ####
-dataA<- get.data("http://host.docker.internal:3032/mydataset/query")
+dataA<- get.data("http://localhost:3030/mydataset/query")
+args <- commandArgs(trailingOnly = TRUE)
+config.file <-args[1]
+print("Config file:")
+print(config.file)
 
 #run analysis over data set A
-localA <- get.local.transmission(dataA)
+localA <- get.local.transmission(dataA,config.file)
 
 print(localA)
-saveRDS(localA, 'algorithmOutput.rds')
+saveRDS(localA, 'algorithmOutput123456.rds')
