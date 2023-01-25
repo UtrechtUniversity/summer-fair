597a5dc38ee165afa60b9e7309dc6674ec446eac
eslavco <e.slavco@gmail.com>
2023-01-25 18:14:57 +0100

    updated function to deal with NA

--- src/R/DataQuality.R
+++ src/R/DataQuality.R
@@ -12,12 +12,14 @@ library(dplyr)
 
 outlier_count <- function(data) {
   data$sample_measure <-as.numeric(data$sample_measure)
-  Q1 <- quantile(data$sample_measure, 0.25)
-  Q3 <- quantile(data$sample_measure, 0.75)
-  lower.bound.mild <- Q1 - 1.5*IQR(data$sample_measure)
-  upper.bound.mild <- Q3 + 1.5*IQR(data$sample_measure)
-  lower.bound.extreme <- Q1 - 3*IQR(data$sample_measure)
-  upper.bound.extreme <- Q3 + 3*IQR(data$sample_measure)
+  Q1 <- quantile(data$sample_measure, 0.25,na.rm=TRUE)
+  Q3 <- quantile(data$sample_measure, 0.75,na.rm=TRUE)
+  lower.bound.mild <- Q1 - 1.5*IQR(data$sample_measure, na.rm = TRUE)
+  upper.bound.mild <- Q3 + 1.5*IQR(data$sample_measure,na.rm = TRUE)
+  lower.bound.extreme <- Q1 - 3*IQR(data$sample_measure, na.rm = TRUE)
+  upper.bound.extreme <- Q3 + 3*IQR(data$sample_measure,na.rm = TRUE)
+  
+  data$sample_measure[is.na(data$sample_measure)]<-0
   
   extreme.outliers <- sum(data$sample_measure < lower.bound.extreme | data$sample_measure > upper.bound.extreme)
   extreme.outliers.perc <- round(extreme.outliers/length(data$sample_measure)*100, digits = 2) 
@@ -43,16 +45,16 @@ outlier_count <- function(data) {
 
 
 
-missing_times <- function(data){
+sample_times <- function(data){
   ## TODO: what makes sense to have here
   # per host, get a list of all times with a result
   # 0 = susceptible, 1= latent, 2 = infectious : is this correct?
   # so the inoculated animals are 1 at time 1??    
-  data$ex_day = as.numeric(data$ex_day)
   
+  data$sample_measure<-as.character(data$sample_measure)
   # list hosts that are inoculated: latent at start experiment?
   inoculatedHosts <- subset(data,
-                            ex_day == 1 & inoculationStatus == 1,
+                            ex_day == 0 & (inoculationStatus == '2' | inoculationStatus =='I' | inoculationStatus=='Inoculated'),
                             select = host_id)
   # split dataframe base on host ID
   inoculated <- subset(data,
@@ -62,22 +64,24 @@ missing_times <- function(data){
   # idea 1: do not group per host, just use all the sampling days.
   days_I <- inoculated %>%
     # remove rows without result:
-    filter(!is.na(sample_measure) | !is.na(sample_result)) %>%
+    filter(!is.na(sample_measure) | !is.na(sample_result) | !is.na(detectionLimit)) %>%
     select(ex_day) %>%
     # make day range of 4 days
+  #  step <-get.step(data$ex_day)
     mutate(range = cut(ex_day, 
-                       breaks = seq(min(ex_day), max(ex_day), by=4), 
+                       breaks = seq(min(ex_day), max(ex_day), by=get.step(length(unique(ex_day)))), 
                        include.lowest=TRUE)) %>%
     # count number of results per day range
     group_by(range) %>%
     summarise(number_of_results = n())
   days_S <- susceptible %>%
     # remove rows without result:
-    filter(!is.na(sample_measure) | !is.na(sample_result)) %>%
+    filter(!is.na(sample_measure) | !is.na(sample_result) | !is.na(detectionLimit)) %>%
     select(ex_day) %>%
     # make day range of 4 days
+#    step <-
     mutate(range = cut(ex_day, 
-                       breaks = seq(min(ex_day), max(ex_day), by=4), 
+                       breaks = seq(min(ex_day), max(ex_day), by=get.step(length(unique(ex_day)))), 
                        include.lowest=TRUE)) %>%
     # count number of results per day range
     group_by(range) %>%
@@ -110,13 +114,24 @@ report.DataQuality <-function(data){
   #data.quality$IQR <- IQR(data$sample_measure)
   data.quality$UnitOfMeasurement <- unique(data$unit_measure)
   data.quality$QuantityFunction <- unique(data$quantity_function)
-  data.quality$outliers.sample.measure <- outlier_count(data)
-  data.quality$MissingSamplesInoculated <- missing_times(data)[1]
-  data.quality$MissingSamplesSusceptible <- missing_times(data)[2]
+  if(all(!is.na(data$sample_measure))){
+  data.quality$outliers.sample.measure <- outlier_count(data)}
+  if (length(unique(data$ex_day))>1){
+  data.quality$SamplesInoculated <- sample_times(data)[1]
+  data.quality$SamplesSusceptible <- sample_times(data)[2]}
    
   return(data.quality)
 }
 
-
+get.step<-function(days_len){
+  mid <- days_len/2
+   
+  if (mid<1){
+    return(0)
+  }
+  else{
+    return(1 + get.step(mid) )
+  }
+}
 
 
